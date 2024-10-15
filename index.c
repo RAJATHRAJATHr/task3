6a6990e29e572f0ce4f8abdae85e43db483c8aad
RAJATHRAJATHr <rajathrvenkitesh@gmail.com>
2024-10-15 23:24:57 +0530

    task 3 uploaded

---
+++ index.c
@@ -0,0 +1,15 @@
+
+#include <iostream >
+
+int main() {
+    int num1, num2, sum;
+
+    std::cout << "Enter two integers: ";
+    std::cin >> num1 >> num2;
+
+    sum = num1 + num2;
+
+    std::cout << "Sum: " << sum << std::endl;
+
+    return 0;
+}
